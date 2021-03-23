8f3365e50b93a7124266ec99430fbe8128fb717e
Steph_Liaw <stephliaw2909@gmail.com>
2021-03-23 14:21:45 +1100

    Changed input speed to 10

--- controllers/NMP_Controller/build/release/NMP_Controller.d
+++ controllers/NMP_Controller/build/release/NMP_Controller.d
@@ -1,12 +1,12 @@
 build/release/NMP_Controller.o: NMP_Controller.cpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Lidar.hpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/shake/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:/Users/shake/AppData/Local/Programs/Webots/include/controller/c/webots/lidar_point.h \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
- C:/Users/shake/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Accelerometer.hpp \
- C:\Users\shake\AppData\Local\Programs\Webots/include/controller/cpp/webots/Gyro.hpp
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Lidar.hpp \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/Stephanie/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:/Users/Stephanie/AppData/Local/Programs/Webots/include/controller/c/webots/lidar_point.h \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Users/Stephanie/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ C:\Users\Stephanie\AppData\Local\Programs\Webots/include/controller/cpp/webots/Gyro.hpp
