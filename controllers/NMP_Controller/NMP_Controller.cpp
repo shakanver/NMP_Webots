1f82efdcd8a11c44266eb23011bdccded0ae5fe2
Steph_Liaw <stephliaw2909@gmail.com>
2021-03-23 15:34:21 +1100

    Created Code to manually control Robot

--- controllers/NMP_Controller/NMP_Controller.cpp
+++ controllers/NMP_Controller/NMP_Controller.cpp
@@ -13,6 +13,7 @@
 #include <webots/DistanceSensor.hpp>
 #include <webots/Camera.hpp>
 #include <webots/Accelerometer.hpp>
+#include <webots/Keyboard.hpp>
 #include <webots/Gyro.hpp>
 #include <limits>
 #include <tuple>
@@ -30,6 +31,14 @@ using namespace webots;
 
 #define INPUT_SPEED 10
 #define SENSOR_ARRAY_LENGTHS 3
+#define MAX_MOTOR_SPEED 3
+
+void redManualMove(const int key, webots::Motor *leftMotor, webots::Motor *rightMotor);
+void moveBackward(webots::Motor *leftMotor, webots::Motor *rightMotor);
+void turnRight(webots::Motor *leftMotor, webots::Motor *rightMotor);
+void turnLeft(webots::Motor *leftMotor, webots::Motor *rightMotor);
+void moveForward(webots::Motor *leftMotor, webots::Motor *rightMotor);
+void stop(webots::Motor *leftMotor, webots::Motor *rightMotor);    
 
 int main(int argc, char **argv) {
   // create the Robot instance.
@@ -40,7 +49,7 @@ int main(int argc, char **argv) {
   //  Motor *motor = robot->getMotor("motorname");
   //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
   //  ds->enable(timeStep);
-  Lidar *lidar = robot->getLidar("lidar");
+  //Lidar *lidar = robot->getLidar("lidar");
   Motor *lmotor = robot->getMotor("left wheel motor");
   Motor *rmotor = robot->getMotor("right wheel motor");
   DistanceSensor *frontLeftDs = robot->getDistanceSensor("ds2");
@@ -50,30 +59,35 @@ int main(int argc, char **argv) {
   DistanceSensor *leftFrontLeftDs = robot->getDistanceSensor("ds5");
   DistanceSensor *rightFrontRightDs= robot->getDistanceSensor("ds4");
   Camera * cam = robot->getCamera("camera");
-  Accelerometer * accelerometer = robot->getAccelerometer("accelerometer");
-  Gyro *gyro = robot->getGyro("gyro");
+  Keyboard *keyboard = robot->getKeyboard();
+  //Accelerometer * accelerometer = robot->getAccelerometer("accelerometer");
+  //Gyro *gyro = robot->getGyro("gyro");
+      
 
   // get the time step of the current world.
   int timeStep = (int)robot->getBasicTimeStep();
 
 
   // Enable the sensors, feel free to change the sampling rate
-  lidar->enable(50);
+  //lidar->enable(50);
   frontLeftDs->enable(timeStep);
   frontRightDs->enable(timeStep);
   leftDs->enable(timeStep);
   rightDs->enable(timeStep);
   leftFrontLeftDs->enable(timeStep);
   rightFrontRightDs->enable(timeStep);
-  accelerometer->enable(timeStep);
-  gyro->enable(timeStep);
+  //accelerometer->enable(timeStep);
+  //gyro->enable(timeStep);
   cam->enable(timeStep);
+  keyboard->enable(timeStep);
   
   lmotor->setPosition(std::numeric_limits<double>::infinity());
   rmotor->setPosition(std::numeric_limits<double>::infinity());
   lmotor->setVelocity(0);
   rmotor->setVelocity(0);
+  int key {-1};
 
+  /*
   //double gain = 0.005;
   //double left_dist = 0.0; //distance value of left sensor
   //double right_dist = 0.0; //distance valur of right sensor
@@ -88,34 +102,18 @@ int main(int argc, char **argv) {
   double left_avg = 0;
   double right_avg = 0;
   double avg_sensor_diff = 0;
+  */
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(timeStep) != -1) {
-  
-    std::cout << "left velocity: " << left_speed << std::endl;
-    std::cout << "right velocity: " << right_speed << std::endl;
-    std::cout << "diff: " << avg_sensor_diff << std::endl;
-    
-    lmotor->setVelocity(left_speed);
-    rmotor->setVelocity(right_speed);
-
-    //initialise left and right sensor arrays with sensor values
-    left_distances[0] = leftDs->getValue();
-    left_distances[1] = leftFrontLeftDs->getValue();
-    left_distances[2] = frontLeftDs->getValue();
-
-    right_distances[0] = rightDs->getValue();
-    right_distances[1] = rightFrontRightDs->getValue();
-    right_distances[2] = frontRightDs->getValue();
-
-
-    left_avg = ((left_distances[0] * 0.2) + (left_distances[1]*0.5) + (left_distances[2]*0.75))/SENSOR_ARRAY_LENGTHS;
-    right_avg = ((right_distances[0]*0.2) + (right_distances[1]*0.5)+ (right_distances[2]*0.75))/SENSOR_ARRAY_LENGTHS;
-    avg_sensor_diff  = left_avg - right_avg;
-
-    left_speed = INPUT_SPEED + (avg_sensor_diff * gain);
-    right_speed = INPUT_SPEED - (avg_sensor_diff * gain);
+        const int prevKey = key;
+        key = keyboard->getKey();
+        if(key != prevKey){
+            lmotor->setPosition(INFINITY);
+            rmotor->setPosition(INFINITY);
+            redManualMove(key, lmotor, rmotor);
+        }
     
   };
 
@@ -124,3 +122,57 @@ int main(int argc, char **argv) {
   delete robot;
   return 0;
 }
+
+//Settng keys to move the robot manually
+void redManualMove(const int key, webots::Motor *leftMotor, webots::Motor *rightMotor){
+    switch(key){
+        case Keyboard::UP:
+            moveForward(leftMotor, rightMotor);
+            break;
+        case Keyboard::DOWN:
+            moveBackward(leftMotor, rightMotor);
+            break;
+        case Keyboard::LEFT:
+            turnLeft(leftMotor, rightMotor);
+            break;
+        case Keyboard::RIGHT:
+            turnRight(leftMotor, rightMotor);
+            break;
+        case ' ':
+            stop(leftMotor, rightMotor);
+            break;
+
+    }
+}
+
+
+void moveForward(webots::Motor *leftMotor, webots::Motor *rightMotor){
+    leftMotor->setVelocity(MAX_MOTOR_SPEED);
+    rightMotor->setVelocity(MAX_MOTOR_SPEED);
+
+}
+
+void moveBackward(webots::Motor *leftMotor, webots::Motor *rightMotor){
+    leftMotor->setVelocity(-1 *MAX_MOTOR_SPEED);
+    rightMotor->setVelocity(-1 *MAX_MOTOR_SPEED);
+}
+
+void turnRight(webots::Motor *leftMotor, webots::Motor *rightMotor){
+    
+    // set up the motor speeds at 10% of the MAX_MOTOR_SPEED.
+    leftMotor->setVelocity(1*MAX_MOTOR_SPEED);
+    rightMotor->setVelocity(0.5*MAX_MOTOR_SPEED);  
+}
+
+void turnLeft(webots::Motor *leftMotor, webots::Motor *rightMotor){
+
+    leftMotor->setVelocity(0.5*MAX_MOTOR_SPEED);
+    rightMotor->setVelocity(1*MAX_MOTOR_SPEED);
+}
+void stop(webots::Motor *leftMotor, webots::Motor *rightMotor){
+
+    leftMotor->setVelocity(0.0);
+    rightMotor->setVelocity(0.0);
+}
+
+    
