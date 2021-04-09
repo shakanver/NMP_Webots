faa72c23b4e62048615b674e185036ed944bee13
Liam <liamgarde.lg@gmail.com>
2021-04-09 22:36:40 +1000

    attempt to merge with /dev

---
+++ controllers/Lidar_Controller/build/release/Lidar_Controller.d
@@ -0,0 +1,15 @@
+build/release/Lidar_Controller.o: Lidar_Controller.cpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Lidar.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ D:/Program\ Files/Webots/include/controller/c/webots/lidar_point.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Accelerometer.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Keyboard.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Gyro.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/display.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/ImageRef.hpp
